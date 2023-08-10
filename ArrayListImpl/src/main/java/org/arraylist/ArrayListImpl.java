8b594c6be88fc0b04c1a3a0869b38c48eee88b8b
Olga Lanz <ext_ssmirnov>
2023-08-10 17:08:26 +0300

    features: 1) optimize ArrayListImpl add function; 2) remove(); 5) indexOf(); 4) invokeArrayCopy(); 3) checkFillSizeArray(); 6) retainAll(); 7) removeAll(); 8) containsAll(); 9) checkIndexOfBounds(); 10) addAll(); 11) remove();

--- ArrayListImpl/src/main/java/org/arraylist/ArrayListImpl.java
+++ ArrayListImpl/src/main/java/org/arraylist/ArrayListImpl.java
@@ -21,10 +21,16 @@ public class ArrayListImpl<E> implements List<E> {
   public ArrayListImpl(int initialCapacity) {
     list = (E[]) new Object[capacity = initialCapacity];
   }
+  
+  public ArrayListImpl(E[] array){
+    this();
+    checkFillSizeArray(count + array.length);
+    invokeArrayCopy(array, 0, list, 0, array.length);
+  }
 
   @Override
   public int size() {
-    return list.length;
+    return count;
   }
 
   @Override
@@ -34,6 +40,10 @@ public class ArrayListImpl<E> implements List<E> {
 
   @Override
   public boolean contains(Object o) {
+    for (E item : list) {
+      if (o.equals(item))
+        return true;
+    }
     return false;
   }
 
@@ -44,7 +54,7 @@ public class ArrayListImpl<E> implements List<E> {
 
       @Override
       public boolean hasNext() {
-        return count + 1 <= size();
+        return count + 1 <= list.length;
       }
 
       @Override
@@ -61,7 +71,7 @@ public class ArrayListImpl<E> implements List<E> {
 
   @Override
   public boolean add(E o) {
-    if (isFill()) {
+    if (isFill(count)) {
       copyList();
     }
     list[count++] = o;
@@ -73,32 +83,21 @@ public class ArrayListImpl<E> implements List<E> {
     int index = 0;
     list = (E[]) new Object[capacity += 10];
     for (E[] array : arrays) {
-      System.arraycopy(array, 0, list, index, array.length);
+      invokeArrayCopy(array, 0, list, index, array.length);
       index += array.length;
     }
   }
 
-
   private E[][] createOldArray(int countOldList) {
     int capacityArrays;
     int fromIndex = 0;
-    E[][] arrays = (E[][]) new Object[countOldList][capacityArrays = count/countOldList];
+    E[][] arrays = (E[][]) new Object[countOldList][capacityArrays = count / countOldList];
     for (int i = 0; i < countOldList; i++) {
-      arrays[i] = Arrays.copyOfRange(list, fromIndex, capacityArrays < size() ? capacityArrays : size());
-      fromIndex = capacityArrays - 1;
-      capacityArrays += arrays[i].length;
+      arrays[i] = Arrays.copyOfRange(list, fromIndex, capacityArrays < list.length ? capacityArrays : list.length);
+      fromIndex = capacityArrays;
+      capacityArrays += arrays[i].length + 1;
     }
     return arrays;
-  }
-
-  private int getCapacityArrays(int countOldList){
-    int capacityArrays = count;
-    int countArray = 1;
-    while(capacityArrays % countOldList != 0){
-      capacityArrays = capacityArrays / countOldList;
-      countArray++;
-    }
-    return capacityArrays;
   }
 
   private int getCountOldArray(int count) {
@@ -107,23 +106,47 @@ public class ArrayListImpl<E> implements List<E> {
       .intValue();
   }
 
-  private boolean isFill() {
-    return (Objects.equals(count, size()));
+  private boolean isFill(int count) {
+    return count >= list.length;
   }
 
   @Override
   public boolean remove(Object o) {
+    for (int i = 0; i < size(); i++) {
+      if (o.equals(list[i])) {
+        remove(i);
+        count--;
+        return true;
+      }
+    }
     return false;
   }
 
   @Override
   public boolean addAll(Collection c) {
-    return false;
+    if (c.isEmpty())
+      return false;
+    else {
+      checkFillSizeArray(count);
+      invokeArrayCopy((E[]) c.toArray(), 0, list, count, c.size());
+      count += c.size();
+      return true;
+    }
   }
 
   @Override
   public boolean addAll(int index, Collection c) {
-    return false;
+    if (c.isEmpty())
+      return false;
+    else {
+      checkFillSizeArray(count + c.size());
+      E[] subList = Arrays.copyOfRange(list, index, size());
+      invokeArrayCopy((E[]) c.toArray(), 0, list, count - subList.length, c.size());
+      checkFillSizeArray(count = (count - subList.length) + c.size());
+      invokeArrayCopy(subList, 0, list, count, subList.length);
+      count += subList.length;
+      return true;
+    }
   }
 
   @Override
@@ -135,76 +158,208 @@ public class ArrayListImpl<E> implements List<E> {
 
   @Override
   public E get(int index) {
-    if (count - 1 >= index) {
-      return list[index];
-    } else throw new ArrayIndexOutOfBoundsException();
+    checkIndexOfBounds(index);
+
+    return list[index];
   }
 
   @Override
   public E set(int index, E element) {
-    if (isIndexOutBounds(index)) {
-      list[index] = element;
-      return element;
-    } else throw new ArrayIndexOutOfBoundsException();
+    checkIndexOfBounds(index);
+    list[index] = element;
+
+    return element;
   }
 
   private boolean isIndexOutBounds(int index) {
-    return count - 1 >= index;
+    return index >= count;
   }
 
   @Override
   public void add(int index, E element) {
+    checkIndexOfBounds(index);
+    checkFillSizeArray(count);
 
+    invokeArrayCopy(list, index, list, index + 1, size());
+    count++;
+    set(index, element);
+  }
+
+  private void checkIndexOfBounds(int index) {
+    if (isIndexOutBounds(index))
+      throw new IndexOutOfBoundsException();
+  }
+
+  private void checkFillSizeArray(int count) {
+    if (isFill(count)) {
+      copyList();
+    }
   }
 
   @Override
   public E remove(int index) {
-    return null;
+    checkIndexOfBounds(index);
+    checkFillSizeArray(count);
+
+    invokeArrayCopy(list, index + 1, list, index, size() - index);
+    list[count] = null;
+    count--;
+    return list[index];
+  }
+
+  private void invokeArrayCopy(E[] src, int srcPoc, E[] dest, int destPoc, int length) {
+    System.arraycopy(src, srcPoc, dest, destPoc, length);
   }
 
   @Override
   public int indexOf(Object o) {
-    return 0;
+    E item = (E) o;
+    for (int i = 0; i < count; i++) {
+      if (list[i].equals(item)) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   @Override
   public int lastIndexOf(Object o) {
-    return 0;
+    E item = (E) o;
+    for (int i = count - 1; i > 0; i--) {
+      if (list[i].equals(item)) {
+        return i;
+      }
+    }
+    return -1;
   }
 
+  //TODO
   @Override
-  public ListIterator listIterator() {
-    return null;
+  public ListIterator<E> listIterator() {
+    return getListIterator();
   }
 
+  //TODO
   @Override
-  public ListIterator listIterator(int index) {
-    return null;
+  public ListIterator<E> listIterator(int index) {
+    return getListIterator();
   }
 
+  //TODO
+  private ListIterator<E> getListIterator() {
+    return new ListIterator<>() {
+      private int count = 0;
+
+      @Override
+      public boolean hasNext() {
+        return count + 1 <= list.length;
+      }
+
+      @Override
+      public E next() {
+        return list[count++];
+      }
+
+      @Override
+      public boolean hasPrevious() {
+        return count > 0;
+      }
+
+      @Override
+      public E previous() {
+        return list[count - 1];
+      }
+
+      @Override
+      public int nextIndex() {
+        return count + 1;
+      }
+
+      @Override
+      public int previousIndex() {
+        return count - 1;
+      }
+
+      @Override
+      public void remove() {
+        ArrayListImpl.this.remove(count);
+      }
+
+      @Override
+      public void set(E e) {
+        set(e);
+      }
+
+      @Override
+      public void add(E e) {
+        add(e);
+      }
+    };
+  }
+
+  //TODO
   @Override
   public List subList(int fromIndex, int toIndex) {
     return null;
   }
 
+  //TODO
   @Override
   public boolean retainAll(Collection c) {
-    return false;
+    int action = 0;
+    for (int i = 0; i < size(); i++) {
+      int count = 0;
+      for (Object item : c) {
+        if (!item.equals(list[i])) {
+          count++;
+        }
+      }
+      if (count == c.size()) {
+        action++;
+        checkFillSizeArray(count);
+        remove(i);
+      }
+    }
+    return action > 0;
   }
 
+  //TODO
   @Override
   public boolean removeAll(Collection c) {
-    return false;
+    if (c.isEmpty())
+      return false;
+    int action = 0;
+    for (Object item : c) {
+      int index = 0;
+      while (index < size()) {
+        if (item.equals(list[index])) {
+          action++;
+          remove(index);
+        }
+        if (!item.equals(list[index]))
+          index++;
+      }
+    }
+    return action != 0;
   }
 
   @Override
   public boolean containsAll(Collection c) {
-    return false;
+    int count = 0;
+    for (Object item : c) {
+      for (E itemArray : this) {
+        if (item.equals(itemArray)) {
+          count++;
+          break;
+        }
+      }
+    }
+    return c.size() == count;
   }
 
   @Override
-  public E[] toArray(Object[] a) {
-    return (E[]) new Object[0];
+  public Object[] toArray(Object[] a) {
+    return Arrays.copyOf(list, count, a.getClass());
   }
 
   @Override
@@ -226,7 +381,7 @@ public class ArrayListImpl<E> implements List<E> {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     ArrayListImpl<?> arrayList = (ArrayListImpl<?>) o;
-    return capacity == arrayList.capacity && count == arrayList.count && Arrays.equals(list, arrayList.list);
+    return Arrays.equals(list, arrayList.list);
   }
 
   @Override
